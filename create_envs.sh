70e9bc82f14c51a8f54562c446295148afecc793
Dkitchen30 <90534334+Dkitchen30@users.noreply.github.com>
2024-12-04 22:07:55 -0500

    Update create_envs.sh

--- create_envs.sh
+++ create_envs.sh
@@ -10,8 +10,12 @@ conda-env list
 vers=10,11,12
 subver=9
 source activate base
-conda install --yes mamba
-mamba install --yes anaconda::jupyter anaconda::spyder
+if [ E`which mamba` == E ]; then
+  conda install --yes mamba
+fi
+if [ E`which jupyter` == E ];then
+  mamba install --yes anaconda::jupyter anaconda::spyder
+fi
 # lists of modules to be installed
 #  if gpu's involved then cudalib=11.8
 cuda="" 
@@ -21,13 +25,13 @@ abinitiomo="psi4 resp"
 openmm="openmm openmmforcefields ambertools mdtraj"
 #abinitio=
 # pip install git+https://github.com/Mishima-syk/psikit # edit after installation
-for v in vers
+for v in ${vers//,/ }
 do
   fv=3.$v
   label=py3$v
   ename=${rootname}${label}
   echo "conda create --yes python=$fv -n $ename >& create_${ename}.LOG"
-  quit
+
   conda create --yes python=$fv -n $ename >& create_${ename}.LOG
   mamba install --yes -n $ename $starter >& starter_${ename}.LOG
   mamba install --yes -n $ename $cheminformatics >& cheminformatics_${ename}.LOG
