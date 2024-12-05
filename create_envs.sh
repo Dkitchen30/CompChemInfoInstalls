140be4c794f0a307c6044bda2baabbb3919215c9
Dkitchen30 <90534334+Dkitchen30@users.noreply.github.com>
2024-12-04 21:45:52 -0500

    Update create_envs.sh

--- create_envs.sh
+++ create_envs.sh
@@ -12,11 +12,27 @@ subver=9
 source activate base
 conda install --yes mamba
 mamba install --yes anaconda::jupyter anaconda::spyder
+# lists of modules to be installed
+#  if gpu's involved then cudalib=11.8
+cuda="" 
+starter=numpy pandas tqdm seaborn matplotlib jupyterlab scikit-learn yellowbrick debtcollector conda-forge::pymol-open-source spyder
+cheminformatics=rdkit mols2grid openbabel biopython
+abinitiomo=psi4 resp
+openmm=openmm openmmforcefields ambertools mdtraj
+#abinitio=
+# pip install git+https://github.com/Mishima-syk/psikit # edit after installation
 for v in vers
 do
   fv=3.$v
   label=py3$v
   ename=${rootname}${label}
-  conda create --yes python=$fv -name $ename >& create_${ename}.LOG
+  conda create --yes python=$fv -n $ename >& create_${ename}.LOG
+  mamba install --yes -n $ename $starter >& starter_${ename}.LOG
+  mamba install --yes -n $ename $cheminformatics >& cheminformatics_${ename}.LOG
+  mamba install --yes -n $ename $abinitiomo >& abinitiomo_${ename}.LOG
+  mamba install --yes -n $ename $openmm >& openmm_${ename}.LOG
+  source activate $ename
+  pip install git+https://github.com/Mishima-syk/psikit
+done
   
   
